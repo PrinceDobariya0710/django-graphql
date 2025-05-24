c0f6782afda2029c67555e8c74dc84339d0df04c
Princekumar <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-05-24 23:00:36 +0530

    Main code

---
+++ books/schema.py
@@ -0,0 +1,24 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from .models import Book
+
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Book
+        fields = "__all__"
+        filter_fields = {
+            "title": ["exact", "icontains", "istartswith"],
+            "author": ["exact", "icontains"],
+            "year": ["exact", "gt", "lt"],
+        }
+        interfaces = (graphene.relay.Node,)
+
+
+class Query(graphene.ObjectType):
+    book = graphene.relay.Node.Field(BookType)
+    all_books = DjangoFilterConnectionField(BookType)
+
+
+schema = graphene.Schema(query=Query)
